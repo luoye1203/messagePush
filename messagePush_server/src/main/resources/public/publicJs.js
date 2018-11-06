b2948d43b336c119a6c79b94c0a7ca9883b563b9
lihongtao <373309891@qq.com>
2018-11-06 11:13:57 +0800

    更改ws的url配置形式

---
+++ messagePush_server/src/main/resources/public/publicJs.js
@@ -0,0 +1,7 @@
+var ip="10.4.136.182";
+var port="8080";
+var appName="messagePush_server";
+var uri="chat";
+
+// var wsurl="ws://"+ip+":"+port+"/"+appName+"/"+uri;
+var wsurl="ws://"+ip+":"+port+"/"+uri;
